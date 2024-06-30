fcb4cfa80062acb705941fd72961500d1b250041
☆ ミーレイ <mirei@noMacBook-Air.local>
2024-06-30 22:50:25 +0900

    Victory message added

--- hello.py
+++ hello.py
@@ -1,11 +1,17 @@
 import random
 
 print("Tossing a coin...")
-result = {"Heads": 0, "Tails": 0}
+results = {"Heads": 0, "Tails": 0}
 
 for i in range(3):
     result = random.choice(["Heads", "Tails"])
-    result[result] += 1
+    results[result] += 1
     print(f"Round {i + 1}: {result}")
 
 print(f"Heads: {results['Heads']}, Tails: {results['Tails']}")
+
+if results["Heads"] > results["Tails"]:
+    print("You won!")
+else:
+    print("You lost!")
+
